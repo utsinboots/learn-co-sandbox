b6dbdf31caefdedb2d20e8be5cf6ac4e70ed23c3
Utshant Gurung <utshantgurung@gmail.com>
2021-06-15 17:28:11 +0000

    Automatically backed up by Learn

---
+++ app.rb
@@ -0,0 +1,9 @@
+def greet_user(name)
+  puts "Hello, #{name}."
+end
+greet_user("Fox")
+
+def greeting(language="Ruby", name)
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
+greeting("Sophie")
